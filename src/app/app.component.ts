f6eb648ed898314b0485fa5458c9e6bcfda5976c
Laptop Nimatuj <1995coral@hotmail.es>
2017-11-03 23:39:52 -0600

    Primer comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MapasService } from './services/mapas.service';
+import { Marcador } from './interfaces/marcador.interface';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  lat: number = 14.834355;
+  lng: number = -91.518305;
+  zoom:number= 16;
+  marcadorSel: any = null;
+  draggable:string= "1";
+ constructor(private _ms: MapasService){
+ 		this._ms.cargarMarcadores();
+ }
+  clickMapa(evento){
+  	console.log(evento);
+  	let nuevoMarcador: Marcador = {
+  		lat: evento.coords.lat,
+  		lng: evento.coords.lng,
+  		titulo: "Sin título",
+  		draggable: true
+  	}
+  	this._ms.insertarMarcador(nuevoMarcador);
+  }
+  clickMarcador(marcador:Marcador, i:number){
+  	console.log(marcador, i);
+  	this.marcadorSel = marcador;
+  	if(this.marcadorSel.draggable)
+  		this.draggable = "1"
+  	else
+  		this.draggable = "0"
+  }
+  dragEndMarcador(marcador:Marcador, evento){
+  	console.log(marcador, evento);
+  	let lat = evento.coords.lat;
+  	let lng = evento.coords.lng;
+  	marcador.lat = lat;
+  	marcador.lng = lng;
+  	this._ms.guardarMarcadores();
+  }
+  borrarMarcador(idx:number){
+  	this._ms.borrarMarcador(idx);
+  	this.marcadorSel = null;
+  }
+  cambiarDraggable(){
+  	console.log(this.draggable);
+  	if(this.draggable=="1"){
+  		this.marcadorSel.draggable = true;
+
+  	}
+  	else{
+  		this.marcadorSel.draggable = false;
+  	}
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
+//Servicios
+import { MapasService } from './services/mapas.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAWIWT5lv80hwskSGgqeoRCHKiR5H43KWk'
+    })
   ],
-  providers: [],
+  providers: [
+    MapasService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
